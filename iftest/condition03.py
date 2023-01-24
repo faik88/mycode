d5210aecba48dc051706053fcbaccfefb15c957e
faik88 <fai829@gmail.com>
2023-01-24 20:03:58 +0000

    Learning about conditional if

--- iftest/condition03.py
+++ iftest/condition03.py
@@ -3,8 +3,11 @@ hostname = input("What value should we set for hostname?")
 ## Notice how the next line has changed
 ## here we use the str.lower() method to return a lowercase string
 if hostname.lower() == "mtg":
-    print("The hostname was found to be mtg")
+    print("The hostname was found to be :"+hostname)
     print("hostname matches expected config")
+else:
+    print("hostname is "+hostname+", you didn't enter the correct value")
+
 
 ## Always print out to the user
 print("Exiting the script")
