86efda97d7b44585becd46078edef319822e4222
faik88 <fai829@gmail.com>
2023-01-24 16:07:30 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   List - making selections from complex lists"""
+
+def main():
+
+    # create a list called animal1
+    animal1 = ["tiger","cat","lion"]
+    animal2 = ["snow","white","king"]
+
+    animal3 = []
+
+    print(animal1)
+    print(animal2)
+    print(animal3)
+
+main()
+
+
